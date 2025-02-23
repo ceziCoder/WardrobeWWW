633044a09115e12420ce9f2909492590cb01581a
cezicoder <cezarylachut1981@gmail.com>
2025-02-23 14:23:55 +0100

    + errorpage

--- szafaW/src/App.tsx
+++ szafaW/src/App.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Main } from "./page/Main";
-
+import { ErrorPage } from './page/ErrorPage'
 
 export const App = () => {
+  const shouldRedirect = true
   return (
     <>
       <BrowserRouter basename='/'>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path='*' element={!shouldRedirect ? <ErrorPage /> : <Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
     </>
